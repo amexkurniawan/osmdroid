8733f59ea2982c3494d072cf6eec8184e777d155
rahmat.azwar <rahmat.azwar@iconpln.co.id>
2021-06-16 14:20:40 +0700

    [RK] - Main - set osmdroid map to main activity

--- app/src/main/java/com/example/osmdroid/MainActivity.kt
+++ app/src/main/java/com/example/osmdroid/MainActivity.kt
@@ -2,10 +2,67 @@ package com.example.osmdroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.preference.PreferenceManager
+import androidx.core.app.ActivityCompat
+import org.osmdroid.views.MapView
+import org.osmdroid.config.Configuration.*
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
 
 class MainActivity : AppCompatActivity() {
+
+    private val REQUEST_PERMISSIONS_REQUEST_CODE = 1
+    private lateinit var map : MapView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //handle permissions first, before map is created. not depicted here
+        // This won't work unless you have imported this: org.osmdroid.config.Configuration.*
+        getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
         setContentView(R.layout.activity_main)
+
+        setMapConfiguration()
+        setMapLocation()
+    }
+
+    private fun setMapConfiguration() {
+        map = findViewById<MapView>(R.id.map)
+        map.setTileSource(TileSourceFactory.MAPNIK)
+    }
+
+    private fun setMapLocation() {
+        val mapController = map.controller
+        mapController.setZoom(9.5)
+        val startPoint = GeoPoint(48.8583, 2.2944);
+        mapController.setCenter(startPoint);
+    }
+
+    override fun onResume() {
+        super.onResume()
+        //needed for compass, my location overlays, v6.0.0 and up
+        map.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        //needed for compass, my location overlays, v6.0.0 and up
+        map.onPause()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        val permissionsToRequest = ArrayList<String>();
+        var i = 0;
+        while (i < grantResults.size) {
+            permissionsToRequest.add(permissions[i]);
+            i++;
+        }
+        if (permissionsToRequest.size > 0) {
+            ActivityCompat.requestPermissions(
+                this,
+                permissionsToRequest.toTypedArray(),
+                REQUEST_PERMISSIONS_REQUEST_CODE);
+        }
     }
 }
